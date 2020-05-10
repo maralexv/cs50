dffb00217574ecdc0c8405a89e9fbccd5baa906d
maralexv <scarvesfamily@yahoo.com>
2020-05-10 18:42:12 +0200

    created pset1

---
+++ pset1/mario.c
@@ -0,0 +1,26 @@
+// Build symmetrical pyramids using bricks
+#include<stdio.h>
+#include<cs50.h>
+
+// Introducing funcs, used in main
+
+
+// Main block, executing the program
+int main(int hight)
+{
+
+}
+
+// Ask user for input - hight of puyramids (number of bricks)
+int ask(void)
+{
+
+}
+
+// Build left pyramid
+
+
+// Build right pyramid
+
+
+// Lay a brick
