cfd10df213dfe9b56c9ea44c760c20fac6387d40
maralexv <scarvesfamily@yahoo.com>
2020-05-11 18:18:21 +0200

    Update mario.c

--- pset1/mario.c
+++ pset1/mario.c
@@ -3,10 +3,14 @@
 #include<cs50.h>
 
 // Introducing funcs, used in main
+int ask(void);
+int pyramids(int hight);
+int build_layer(int n, int width);
+int lay_brick(void);
+int keep_void(void);
 
-
-// Main block, executing the program
-int main(int hight)
+// Main block, where the program starts
+int main(void)
 {
 
 }
@@ -14,13 +18,40 @@ int main(int hight)
 // Ask user for input - hight of puyramids (number of bricks)
 int ask(void)
 {
+    int num;
+    do
+    {
+        num = get_int("How many bricks tall do you want the pyramids to be? ");
+    }
+    while (num < 1);
+    return num;
+}
+
+// Build 2 symmetric pyramids
+int pyramids(int hight)
+{
+    for (int i = 1; i < hight; i++)
+    {
+        build_layer(i, hight);
+    }
+}
+
+// Build one layer of the pyramid
+int build_layer(int n, int width)
+{
+    int n_bricks = n;
+    int n_voids = width - n;
 
 }
 
-// Build left pyramid
-
-
-// Build right pyramid
-
-
 // Lay a brick
+int lay_brick(void)
+{
+    printf("#");
+}
+
+// Keep the void
+int keep_void(void)
+{
+    printf(" ");
+}
