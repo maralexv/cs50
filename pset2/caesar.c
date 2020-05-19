b99810c7856619001c8966eab1deaf954b8ea2ab
maralexv <scarvesfamily@yahoo.com>
2020-05-19 14:25:43 +0200

    updated caesar.c

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -1,27 +1,33 @@
 // Encrypts messages using Caesar’s cipher
-#include<stdio.h>
-#include<cs50.h>
-#include<ctype.h>
-#include<string.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
+    // Check whether user provided encription key
+    if (argc < 2)
+    {
+        printf("./caesar key\n");
+        return 1;
+    }
     // Get user input
     string ptext = get_string("plaintext: ");
-    // Encript user input 
-    string ctext;
+    // Encript user input
+    string ctext = ptext;
     for (int i = 0, n = strlen(ptext); i < n; i++)
     {
         if (isalpha(ptext[i]))
         {
-            ctext[i] = (ptext[i] + (int) argv[1]) % 26;
+            ctext[i] = (ptext[i] + (int)argv[1]) % 26;
         }
         else
         {
-            ctext[i]=ptext[i];
+            ctext[i] = ptext[i];
         }
     }
     // Output encripted (ciphered) text
-    printf("ciphertext: %s\n", ctext); 
+    printf("ciphertext: %s\n", ctext);
     return 0;
 }
