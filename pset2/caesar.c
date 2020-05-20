6a7440812c34aae0a75d4195a9b2dea754bcb33a
maralexv <scarvesfamily@yahoo.com>
2020-05-20 16:42:35 +0200

    updated caesar.c

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -1,8 +1,11 @@
 // Encrypts messages using Caesar’s cipher
-#include <stdio.h>
-#include <cs50.h>
-#include <ctype.h>
-#include <string.h>
+#include<stdio.h>
+#include<cs50.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdlib.h>
+
+string cipher(string txt, int k, int pl);
 
 int main(int argc, string argv[])
 {
@@ -12,22 +15,31 @@ int main(int argc, string argv[])
         printf("./caesar key\n");
         return 1;
     }
+    int key = atol(argv[1]);
+    printf("%d\n", key);
     // Get user input
-    string ptext = get_string("plaintext: ");
-    // Encript user input
-    string ctext = ptext;
-    for (int i = 0, n = strlen(ptext); i < n; i++)
+    string plaintext = get_string("plaintext: ");
+    int plaintext_len = strlen(plaintext);
+    // Ecript user input
+    string ciphertext = cipher(plaintext, key, plaintext_len);
+    // Output encripted (ciphered) text
+    printf("ciphertext: %s\n", ciphertext);
+    return 0;
+}
+// Encript string passed to the function
+string cipher(string txt, int k, int pl)
+{
+    string ctext = txt;
+    for (int i = 0; i < pl; i++)
     {
-        if (isalpha(ptext[i]))
+        if (isalpha(txt[i]))
         {
-            ctext[i] = (ptext[i] + (int)argv[1]) % 26;
+            ctext[i] = (txt[i] + k) % 26;
         }
         else
         {
-            ctext[i] = ptext[i];
+            ctext[i] = txt[i];
         }
     }
-    // Output encripted (ciphered) text
-    printf("ciphertext: %s\n", ctext);
-    return 0;
+    return ctext;
 }
