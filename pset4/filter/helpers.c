e4f30d6ab7937cd6fb4cbb85358c266c492a4b9a
maralexv <scarvesfamily@yahoo.com>
2020-07-01 09:05:26 +0200

    inmplemented sepia in filter.c

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -15,18 +15,61 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[h][w].rgbtRed = avg;
         }
     }
+
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h = 0; h < height; ++h)
+    {
+        for (int w = 0; w < width; ++w)
+        {
+            int sepRed = roundf((0.189 * image[h][w].rgbtBlue) + (0.769 * image[h][w].rgbtGreen) + (0.393 * image [h][w].rgbtRed));
+            int sepGreen = roundf((0.168 * image[h][w].rgbtBlue) + (0.686 * image[h][w].rgbtGreen) + (0.349 * image[h][w].rgbtRed));
+            int sepBlue = roundf((0.131 * image[h][w].rgbtBlue) + (0.534 * image[h][w].rgbtGreen) + (0.272 * image[h][w].rgbtRed));
+
+            if (sepRed > 255)
+            {
+                image[h][w].rgbtRed = 255;
+            }
+            else
+            {
+                image[h][w].rgbtRed = sepRed;
+            }
+
+            if (sepGreen > 255)
+            {
+                image[h][w].rgbtGreen = 255;
+            }
+            else
+            {
+                image[h][w].rgbtGreen = sepGreen;
+            }
+
+            if (sepBlue > 255)
+            {
+                image[h][w].rgbtBlue = 255;
+            }
+            else
+            {
+                image[h][w].rgbtBlue = sepBlue;
+            }
+        }
+    }
+
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h = 0; h < height; ++h)
+    {
+        for (int w = 0; w < width/2; ++h)
+    }
+    
     return;
 }
 
