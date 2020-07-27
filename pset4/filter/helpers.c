e4ce99f7ef6fa061a09c1031e801788097e23f55
maralexv <scarvesfamily@yahoo.com>
2020-07-27 06:46:50 +0200

    corrected reft=lect in helper.c

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -1,6 +1,8 @@
 #include "helpers.h"
 #include <math.h>
 
+void swap(RGBTRIPLE *leftpixel, RGBTRIPLE *rightpixel);
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -11,12 +13,11 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         {
             // calculate average of 3 pixel colours
             float total = 0;
-            int arr[3] = 
-            {
-                image[h][w].rgbtBlue, 
-                image[h][w].rgbtGreen, 
-                image[h][w].rgbtRed
-            };
+            int arr[3] =
+                {
+                    image[h][w].rgbtBlue,
+                    image[h][w].rgbtGreen,
+                    image[h][w].rgbtRed};
             for (int i = 0; i < 3; ++i)
             {
                 total += arr[i];
@@ -39,7 +40,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int w = 0; w < width; ++w)
         {
-            int sepRed = roundf((0.189 * image[h][w].rgbtBlue) + (0.769 * image[h][w].rgbtGreen) + (0.393 * image [h][w].rgbtRed));
+            int sepRed = roundf((0.189 * image[h][w].rgbtBlue) + (0.769 * image[h][w].rgbtGreen) + (0.393 * image[h][w].rgbtRed));
             int sepGreen = roundf((0.168 * image[h][w].rgbtBlue) + (0.686 * image[h][w].rgbtGreen) + (0.349 * image[h][w].rgbtRed));
             int sepBlue = roundf((0.131 * image[h][w].rgbtBlue) + (0.534 * image[h][w].rgbtGreen) + (0.272 * image[h][w].rgbtRed));
 
@@ -78,28 +79,33 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE tmp;
+    // RGBTRIPLE tmp;
     int mid;
 
     if (width % 2 == 0)
     {
-        mid = width/2;
+        mid = width / 2;
     }
     else
     {
-        mid = width/2 + 1;
+        mid = width / 2 + 1;
     }
 
     for (int h = 0; h < height; ++h)
     {
         for (int w = 0; w < mid; ++w)
         {
-            tmp = image[h][w];
-            image[h][w] = image[h][width - w];
-            image[h][width - w] = tmp;
+            swap(&image[h][w], &image[h][width - 1 - w]);
         }
     }
     return;
+}
+
+void swap(RGBTRIPLE *leftpixel, RGBTRIPLE *rightpixel)
+{
+    RGBTRIPLE tmp = *leftpixel;
+    *leftpixel = *rightpixel;
+    *rightpixel = tmp;
 }
 
 // Blur image
