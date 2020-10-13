c0ac0241620bef461f9943d17ae0cacab5d75c49
maralexv <scarvesfamily@yahoo.com>
2020-10-13 07:07:56 +0200

    minor updates to pointers.c

--- learn_c/pointers.c
+++ learn_c/pointers.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+
+/*
+Here is how we can declare pointers:
+int* p;
+int * p;
+int *p;
+
+To assign and addres pof a variable to a pointer we use &:
+int *pc;
+int c;
+c = 5
+pc = &c;
+*/
+
 int main()
 {
-  int var = 3;
+  int var = 15;
   printf("var: %d\n", var);
 
   // Notice the use of & before var
