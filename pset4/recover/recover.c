17cf620ddeed8179f00fdea21f4b9c9132ede752
maralexv <scarvesfamily@yahoo.com>
2020-10-14 14:06:49 +0200

    update recover

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -5,18 +5,32 @@ int main(int argc, char *argv[])
 {
     // Check if the arguments passed are as expected
 
-    // Open memery card
+    // Open memery card and save the content into a file
     FILE *f = fopen(card, 'r');
+    if (f == NULL)
+    {
+        perror("Error in opening file");
+        return (1);
+    }
 
-    // Scan card and recover JPEG files
+    // Scan the card file and recover JPEG files
 
-        // Look for JPEG files and recover them
+        // Iterate over blocks of 512 bytes
+        int j = 0;
+        while (1)
+        {
+            // Check if at the end of the file
+            if (feof(f))
+            {
+                printf("End of file.");
+                break;
+            }
+                // Find the 1st block in the file
 
-            // Iterate over blocks of 512 bytes
-            int j = 0;
                 // Check if the beginning of the block has 0xff 0xd8 0xff (0xe0 upto 0xef)
                 int *buff = malloc(sizeof(int) * 512);
                 fread(buff, sizeof(int), 512, f);
+
                 buff[0] == 0xff;
                 buff[0] == 0xd8;
                 buff[0] == 0xff;
@@ -29,11 +43,13 @@ int main(int argc, char *argv[])
                 // Write the content to the opened file
                 fwrite(buff, sizeof(int), 512, filename);
 
-            // Move to the next block, check if the end of card
+                // Move to the next block
 
-            // Go back to check if JPEG, then Close the open file  and repeat
-
-    // Close the program
+            // Go back to begining of the while loop  and repeat
+            j += 1;
+        }
+    // Close the card file
     fclose(f);
 
+return(0);
 }
