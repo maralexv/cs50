53fc623027c4654f8c5fd936ef3d81a5c1b2aa35
maralexv <scarvesfamily@yahoo.com>
2020-10-17 19:13:24 +0200

    Update recover.c

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -4,79 +4,85 @@
 int main(int argc, char *argv[])
 {
     // Check if the arguments passed are as expected
-    if (argc != 1)
+    if (argc != 2)
     {
         printf("Usage: ./recver card.row\n");
-        return (1);
+        return 1;
     }
     // Open memery card for reading
-    FILE *f = fopen(argv[0], 'r');
-    if (f == NULL)
+    FILE *fs = fopen(argv[1], "r");
+    if (fs == NULL)
     {
-        printf("Error opening file");
-        return (1);
+        printf("Error opening file\n");
+        return 1;
     }
 
     // Scan the card file and recover JPEG files
 
         // Get to the beginnig of the file
-        fseek(f, 0, SEEK_SET);
+        fseek(fs, 0, SEEK_SET);
 
         // Iterate over blocks of 512 bytes
         int i = 0;
         int j = 0;
         while (1)
         {
-            // Check if at the end of the file
-            if (feof(f))
+            // Check if at the end of the file and stop the loop if true
+            if (feof(fs))
             {
-                printf("End of fileß");
+                printf("End of file\n");
                 break;
             }
                 // Go to the i-th block in the file
-                fseek(f, (i * 512), SEEK_SET);
+                fseek(fs, (i * 512), SEEK_SET);
 
                 // Check if the beginning of the block has 0xff 0xd8 0xff (0xe0 upto 0xef)
-                int *buff = malloc(sizeof(int) * 512);
-                fread(buff, sizeof(int), 512, f);
+                int *buff = malloc(sizeof(char) * 512);
+                fread(buff, sizeof(char), 512, fs);
 
-                if (buff[0] == 0xff &
-                    buff[1] == 0xd8 &
-                    buff[2] == 0xff &
+                if (buff[0] == 0xff &&
+                    buff[1] == 0xd8 &&
+                    buff[2] == 0xff &&
                     (buff[3] & 0xf0) == 0xe0)
                 {
                     // If true Open (create) new file for appending
-                    j += 1;
+                    ++j;
                     char filename[8];
-                    sprintf(filename, '%03d.jpg', j);
-                    FILE *filename = fopen(filename, 'a');
-                    if (filename == NULL)
+                    sprintf(filename, "%03d.jpg", j);
+                    FILE *fd = fopen(filename, "a");
+                    if (fd == NULL)
                     {
                         printf("Error creating new file");
                         return (1);
                     }
                     // Write the content of the block in buffer to the opened file
-                    fwrite(buff, sizeof(int), 512, filename);
-                    // Move to the end of the block 
-                    fseek(filename, 512, SEEK_CUR);
+                    fwrite(buff, sizeof(char), 512, fd);
+                    fclose(fd);
+                    free(buff);
                 }
                 
                 if (j > 0)
                 {
                     char filename[8];
-                    sprintf(filename, '%03d.jpg', j);
+                    sprintf(filename, "%03d.jpg", j);
+                    // Open fd file to append
+                    FILE *fd = fopen(filename, "a");
+                    if (fd == NULL)
+                    {
+                        printf("Error creating new file");
+                        return (1);
+                    }
                     // Write the content of the block in buffer to the opened file
-                    fwrite(buff, sizeof(int), 512, filename);
-                    // Move to the end of the block
-                    fseek(filename, 512, SEEK_CUR);
+                    fwrite(buff, sizeof(int), 512, fd);
+                    fclose(fd);
                 }
-                
 
             // Go back to begining of the while loop  and repeat
-            i += 1;
+            ++i;
         }
-    // Close the card file
-    fclose(f);
 
-return (0);
+    // Close the card file
+    fclose(fs);
+
+return 0;
 }
