8df3421b29b56fb81554a4dd252131a782dd9756
maralexv <scarvesfamily@yahoo.com>
2020-10-18 18:10:44 +0200

    changed recover.c

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -19,70 +19,51 @@ int main(int argc, char *argv[])
 
     // Scan the card file and recover JPEG files
 
-        // Get to the beginnig of the file
-        fseek(fs, 0, SEEK_SET);
+    // End of the file
+    fseek(fs, 0, SEEK_END);
+    // How many blocks of 512 bytes
+    int nb = ftell(fs) / 512;
+    printf("nb = %d\n", nb);
 
-        // Iterate over blocks of 512 bytes
-        int i = 0;
-        int j = 0;
-        while (1)
+    // Initialise array where we shall store marks of where the jpeg fils start
+    int *marks;
+
+    // Get back to the beginnig of the file
+    fseek(fs, 0, SEEK_SET);
+
+    // Iterate over blocks of 512 bytes
+    for (int i = 0; i < 200; ++i)
+    {
+        // Check if the beginning of the block has 0xff 0xd8 0xff (0xe0 upto 0xef)
+        int *buff = malloc(512);
+        if (buff == NULL)
         {
-            // Check if at the end of the file and stop the loop if true
-            if (feof(fs))
-            {
-                printf("End of file\n");
-                break;
-            }
-                // Go to the i-th block in the file
-                fseek(fs, (i * 512), SEEK_SET);
-
-                // Check if the beginning of the block has 0xff 0xd8 0xff (0xe0 upto 0xef)
-                int *buff = malloc(sizeof(char) * 512);
-                fread(buff, sizeof(char), 512, fs);
-
-                if (buff[0] == 0xff &&
-                    buff[1] == 0xd8 &&
-                    buff[2] == 0xff &&
-                    (buff[3] & 0xf0) == 0xe0)
-                {
-                    // If true Open (create) new file for appending
-                    ++j;
-                    char filename[8];
-                    sprintf(filename, "%03d.jpg", j);
-                    FILE *fd = fopen(filename, "a");
-                    if (fd == NULL)
-                    {
-                        printf("Error creating new file");
-                        return (1);
-                    }
-                    // Write the content of the block in buffer to the opened file
-                    fwrite(buff, sizeof(char), 512, fd);
-                    fclose(fd);
-                    free(buff);
-                }
-                
-                if (j > 0)
-                {
-                    char filename[8];
-                    sprintf(filename, "%03d.jpg", j);
-                    // Open fd file to append
-                    FILE *fd = fopen(filename, "a");
-                    if (fd == NULL)
-                    {
-                        printf("Error creating new file");
-                        return (1);
-                    }
-                    // Write the content of the block in buffer to the opened file
-                    fwrite(buff, sizeof(int), 512, fd);
-                    fclose(fd);
-                }
-
-            // Go back to begining of the while loop  and repeat
-            ++i;
+            printf("Error allocating memroy\n");
+            return 1;
         }
+        // Load 512 bytes into a buffer
+        fread(buff, 1, 512, fs);
+        
+        printf("i = %d, position = %ld, buff[0] = %x \n", i, ftell(fs), buff[0]);
+
+        if (buff[0] == 0xff &&
+            buff[1] == 0xd8 &&
+            buff[2] == 0xff &&
+            (buff[3] & 0xf0) == 0xe0)
+        {
+            // Rercord the position to the marks array
+            fseek(fs, 0, SEEK_CUR);
+            marks = realloc(marks, sizeof(int) + (i * sizeof(int)));
+            marks[i] = ftell(fs);
+        }
+        free(buff);
+        // Go to the next block in the file
+        fseek(fs, 512, SEEK_CUR);
+    }
 
     // Close the card file
     fclose(fs);
+    free(marks);
 
 return 0;
 }
