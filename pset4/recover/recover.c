e3d22df90a6f0dbe0f157c2cd192e3cc942e6992
maralexv <scarvesfamily@yahoo.com>
2020-10-20 07:19:33 +0200

    updated recover

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -17,56 +17,6 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // End of the file
-    fseek(fs, 0, SEEK_END);
-    // How many blocks of 512 bytes
-    int nb = ftell(fs) / 512;
-
-    // Initialise array to store marks of where the jpeg files start
-    int *marks;
-
-    // Get back to the beginnig of the file
-    fseek(fs, 0, SEEK_SET);
-
-    // Iterate over blocks of 512 bytes
-    for (int i = 0; i < nb; ++i)
-    {
-        if(feof(fs))
-        {
-            break;
-        }
-
-        // Check if the beginning of the block has 0xff 0xd8 0xff (0xe0 upto 0xef)
-        int *buff = malloc(512);
-        if (buff == NULL)
-        {
-            printf("Error allocating memroy for buffer\n");
-            return 1;
-        }
-        // Load 512 bytes into a buffer
-        fread(buff, 1, 512, fs);
-
-        if (buff[0] == 0xff &&
-            buff[1] == 0xd8 &&
-            buff[2] == 0xff &&
-            (buff[3] & 0xf0) == 0xe0)
-        {
-            // Rercord the position to the marks array
-            fseek(fs, 0, SEEK_CUR);
-            marks = realloc(marks, sizeof(int) + (i * sizeof(int)));
-            if (marks == NULL)
-            {
-                printf("Error reallocating memory for marks\n");
-                return 1;
-            }
-            marks[i] = ftell(fs);
-        }
-        free(buff);
-    }
-
-    // Close the card file
-    fclose(fs);
-    free(marks);
 
 return 0;
 }
--- pset4/recover/test.c
+++ pset4/recover/test.c
@@ -4,40 +4,71 @@
 int main()
 {
     FILE *f = fopen("card.raw", "r");
-    if (f == NULL)
+    if (!f)
     {
         printf("Error reading the file\n");
         return 1;
     }
-    fseek(f, 0, SEEK_END);
-    printf("size is %ld bytes, 4-byte blocks: %ld.\n", ftell(f), ftell(f) / sizeof(int));
 
     fseek(f, 0, SEEK_SET);
 
-    int i = 0;
+    int i = -1;
     while (1)
     {
         if (feof(f))
         {
             break;
         }
-        int *b = malloc(512);
+
+        unsigned char *b = malloc(512);
         if (b == NULL)
         {
             printf("Error!");
             return 2;
         }
-
         fread(b, 1, 512, f);
 
-        if (b[0] == 0xff)
+        if (b[0] == 0xff && 
+            b[1] == 0xd8 && 
+            b[2] == 0xff && 
+            (b[3] & 0xf0) == 0xe0)
         {
-            printf("i = %d, b[0] = %x, position = %ld.\n", i, b[0], ftell(f));
+            if (i < 1)
+            {
+                ++i;
+                long int start = ftell(f);
+                // Crete new .jpg file and open for append
+                char filename[8];
+                sprintf(filename, "%03i.jpg", i);
+
+                // Write b block into new .jpeg file
+
+                // for testing
+                printf("%s, start: %ld\n", filename, start);
+            }
+            else
+            {
+                // Close the previouse .jpg file 
+
+                // Create new .jpg file and open for append
+
+                // Write b block into new .jpg file
+            }
         }
-        ++i;
+
+        if (i != -1)
+        {
+            // Write b block to the current .jpg file
+        }
+
         free(b);
     }
 
     fclose(f);
     return 0;
 }
+
+/*
+FILE *img = fopen(filename, "w");
+fwrite(data, size, number, file);
+*/
