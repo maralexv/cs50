72e238db950a21e9ff1bcc8affe85caed891ca62
maralexv <scarvesfamily@yahoo.com>
2020-10-23 07:20:47 +0200

    updated recover

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include <stdlib.h>
 
+typedef uint8_t BYTE;
+
 int main(int argc, char *argv[])
 {
     // Check if the arguments passed are as expected
@@ -14,34 +16,36 @@ int main(int argc, char *argv[])
     FILE *sourcefile = fopen(argv[1], "r");
     if (!sourcefile)
     {
-        printf("Error reading the file\n");
+        printf("Error reading the file!`\n");
         return 1;
     }
 
     // Initialise variables to be used in while loop
-    int i = -1;
-    char filename[8];
-    FILE *targetfile;
+    int i = 0; // Images counter
+    int y = 0; // Flag of 1st image fohnd
+    char filename[8]; // Image name
+    FILE *targetfile = NULL; // Target file to store images
+
+    fseek(sourcefile, 0, SEEK_END);
+    unsigned long end = ftell(sourcefile);
+    
     // Allocate memry for a buffer
-    unsigned char *b = malloc(512);
+    BYTE *b = malloc(512);
     if (b == NULL)
     {
-        printf("Error!");
+        fclose(sourcefile);
+        printf("Error allocating memory for buffer!\n");
         return 2;
     }
 
     // Get to the very begining of the file and scan it
     fseek(sourcefile, 0, SEEK_SET);
-    while (1)
-    {
-        // Break out of wjile loop if end of file is reached
-        if (feof(sourcefile))
-        {
-            break;
-        }
 
+    // Loop through the sourcefile by chuncks of 512 bytes
+    do
+    {
         // Read 512 - byte block into buffer
-        fread(b, 1, 512, sourcefile);
+        fread(b, sizeof(BYTE), 512, sourcefile);
 
         // Check the header of the block to see whether it is .jpg file header
         if (b[0] == 0xff && 
@@ -50,54 +54,68 @@ int main(int argc, char *argv[])
             (b[3] & 0xf0) == 0xe0)
         {
             // If first jpg header found
-            if (i < 0)
+            if (!y)
             {
                 // Crete new .jpg file and open for append
-                ++i;
                 sprintf(filename, "%03i.jpg", i);
                 targetfile = fopen(filename, "a");
                 if (!targetfile)
                 {
-                    printf("Error creating the file\n");
+                    fclose(sourcefile);
+                    fclose(targetfile);
+                    printf("Error creating the 1st file!\n");
                     return 3;
                 }
                 // Write b block into new .jpeg file
-                fwrite(b, 1, 512, targetfile);
+                fwrite(b, sizeof(BYTE), 512, targetfile);
+                fclose(targetfile);
+                y = 1;
             }
 
             // If not the 1st jpg header found
             else
             {
-                // Close the previouse .jpg file
-                fclose(targetfile);
                 // Create new .jpg file and open for append
-                ++i;
-                sprintf(filename, "%03i.jpg", i);
+                sprintf(filename, "%03i.jpg", i++);
                 targetfile = fopen(filename, "a");
                 if (!targetfile)
                 {
-                    printf("Error creating the file\n");
-                    return 3;
+                    fclose(sourcefile);
+                    fclose(targetfile);
+                    printf("Error creating new file!\n");
+                    return 4;
                 }
                 // Write b block into new .jpeg file
-                fwrite(b, 1, 512, targetfile);
+                fwrite(b, sizeof(BYTE), 512, targetfile);
+                fclose(targetfile);
             }
         }
 
-        // When a target jpg file is already open
-        if (i > -1)
+        // When a target jpg file already exists
+        if (y)
         {
+            sprintf(filename, "%03i.jpg", i);
+            targetfile = fopen(filename, "a");
+            if (!targetfile)
+            {
+                fclose(sourcefile);
+                fclose(targetfile);
+                printf("Error openning target file!\n");
+                return 5;
+            }
             // Write b block to the current .jpg file
-            fwrite(b, 1, 512, targetfile);
+            fwrite(b, sizeof(BYTE), 512, targetfile);
+            fclose(targetfile);
         }
 
-    }
+    } while (ftell(sourcefile) < end);
+    printf("%d\n", i);
 
+    // Garbage collection
     // Free dynamically allocated memry
     free(b);
-
     // Close all open files and exit
-    fclose(targetfile);
+    // fclose(targetfile);
     fclose(sourcefile);
     return 0;
 }
